4b7c246ca449fd5e49b6093d36f6c382a8aa8b8e
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2023-12-09 02:36:32 -0300

    feat :sparkles: Add components, services and routes

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent, },
+    {path: 'tests', component: TestComponent},
+    {path: 'favorites', component: FavoritesComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {
-        path: '', component: TestComponent
+        path: '**', 
+        component: NotFoundComponent
     }
 ];
